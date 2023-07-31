7ae70b6fba8387c0fb6dc02bd20c47c2126aaa36
Marcos Gardinali <100800469+MarcosGardinali@users.noreply.github.com>
2023-07-31 14:08:40 -0300

    .

--- src/App.js
+++
@@ -1,6 +0,0 @@
-import './styles/estilosGlobais.scss'
-import PaginaInicial from "./Paginas/PaginaInicial";
-
-export default function App() {
-  return <PaginaInicial />
-}
--- src/Paginas/PaginaInicial/index.jsx
+++
@@ -1,8 +0,0 @@
-import Cabecalho from "../../Componentes/Cabecalho";
-
-export default function PaginaInicial(){
-    return(
-        <Cabecalho />
-        
-    )
-}
